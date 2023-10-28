7ca55abce15ab8cd11916a33d53c92859862857b
Manu Emmanuel <manuemmanuel2026@cs.sjcetpalai.ac.in>
2023-10-28 16:16:12 +0530

    Update circularqueue.c

--- circularqueue.c
+++ circularqueue.c
@@ -1,35 +1,32 @@
 #include <stdio.h>
-
 int rear = -1, front = -1, queue[20], max;
-
 int enqueue(int item);
 int dequeue();
 int display();
-
 void main() {
     int choice, num, k = 1;
-
     printf("Enter the maximum size of the queue: ");
     scanf("%d", &max);
-
     while (k != 0) {
-        printf("\n 1. Enqueue\n 2. Dequeue\n 3. Display\n 4. Exit\n Enter a choice: ");
+        printf("\n1.Enqueue\n2.Dequeue\n3.Display\n4.Exit\nEnter a choice: ");
         scanf("%d", &choice);
 
         if (choice == 1) {
             printf("Enter a number: ");
             scanf("%d", &num);
             enqueue(num);
-        } else if (choice == 3) {
+        } 
+        else if (choice == 3) {
             display();
-        } else if (choice == 2) {
+        } 
+        else if (choice == 2) {
             dequeue();
-        } else if (choice == 4) {
+        } 
+        else if (choice == 4) {
             break;
         }
     }
 }
-
 int enqueue(int item) {
     if ((front == 0 && rear == max - 1) || (rear == front - 1 && front != -1)) {
         printf("\nOVERFLOW\n");
@@ -41,7 +38,6 @@ int enqueue(int item) {
         queue[rear] = item;
     }
 }
-
 int dequeue() {
     if (front == -1) {
         printf("\nThe queue is EMPTY.\n");
@@ -55,12 +51,14 @@ int dequeue() {
         }
     }
 }
-
 int display() {
     printf("The queue is: \n");
     int i = front;
-    do {
+    while (i != rear) {
         printf("%d/", queue[i]);
         i = (i + 1) % max;
-    } while (i != (rear + 1) % max);
+    }
+    printf("%d",queue[i]);
+    printf("\nRear: %d",rear);
+    printf("\nFront: %d",front);
 }
